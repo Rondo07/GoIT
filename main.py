49cd130415c05cdd988673714339f205556f7168
Rondo07 <stas.mom07@gmail.com>
2025-10-12 23:06:26 +0100

    Created func to print odd nums

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def print_world():
-    print("Word!")
+def print_only_odd_numbers_in_a_list(number_list):
+    for number in number_list:
+        if number % 2 != 0:
+            print(number)
+        
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print_only_odd_numbers_in_a_list(numbers)
