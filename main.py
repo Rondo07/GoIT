c70a6411eae05cab29c628f70d77aa1c48204acb
Stan <110232998+Rondo07@users.noreply.github.com>
2025-11-03 17:44:26 +0000

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def print_only_odd_numbers_in_a_list(number_list):
-    for number in number_list:
-        if number % 2 != 0:
-            print(number)
-        
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print_only_odd_numbers_in_a_list(numbers)
