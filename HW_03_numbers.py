fdfa60765949389d2f8896872de9543d2cb45b8d
Rondo07 <stas.mom07@gmail.com>
2025-11-04 12:23:08 +0000

    Added check for numbers with +

--- HW_03_numbers.py
+++ HW_03_numbers.py
@@ -1,15 +1,19 @@
 import re
 
 def normalize_phone(phone_number):   # Create a function
-    sanitized_number = re.sub(r'\D',  '', phone_number)   # Remove all non-digit characters
-    if not sanitized_number.startswith('+'):   # Check if number hasn't +
-            if sanitized_number.startswith('380'):   # Check if number starts with 380
-                sanitized_number = '+' + sanitized_number   # Add + to number
-            else:
-                sanitized_number = '+38' + sanitized_number   #Otherwise add +38 to number
+    sanitized_number = re.sub(r'[^\d+]',  '', phone_number.strip())   # Remove all non-digit characters except '+'
+    if sanitized_number.startswith('+'):   # Check if number starts with +
+        return sanitized_number
+    if sanitized_number.startswith('380'):   # Check if number starts with 380
+        sanitized_number =  '+' + sanitized_number   # Add + to number
+    else:
+        sanitized_number = '+38' + sanitized_number   #Otherwise add +38 to number
     return sanitized_number
-    
+
 raw_numbers = [
+    "+44 7911123456",
+    "+49 151-234-567-89",
+    "+33 612 3456 78",
     "067\\t123 4567",
     "(095) 234-5678\\n",
     "+380 44 123 4567",
