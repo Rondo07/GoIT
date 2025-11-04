24803e2d43d3c29fa7a3fe9ef286449535cc3785
Rondo07 <stas.mom07@gmail.com>
2025-11-04 11:26:59 +0000

    Added check for quantity

--- HW_03_random.py
+++ HW_03_random.py
@@ -4,6 +4,9 @@ import random
 def get_numbers_ticket(min, max, quantity):   # Define function
     if min < 1 or max > 1000 or min > max:   # Check the condition for min and max
         return []
+    range_numbers = max - min + 1
+    if quantity > range_numbers:   # Check the condition for quantity > min-max range
+        return []
     
     lottery_numbers = set()   # Create an empty set
     while len(lottery_numbers) < quantity:   # Generate numbers in a while-loop
@@ -11,8 +14,8 @@ def get_numbers_ticket(min, max, quantity):   # Define function
     return sorted(lottery_numbers)
 
 
-lottery_numbers = get_numbers_ticket(1, 1050, 4)
+lottery_numbers = get_numbers_ticket(1, 100, 4)
 print("Ваші лотерейні числа:", lottery_numbers)
 
-lottery_numbers = get_numbers_ticket(1, 100, 4)
+lottery_numbers = get_numbers_ticket(10, 14, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
